65bf36aeeaf30e7bdd9b89b067356e550501194d
deividadrian <deividadrian24@gmail.com>
2022-10-25 20:13:47 -0300

    Adicionar criptografia de senha do usuario

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from "typeorm";
 import User from '../typeorm/entities/User';
 import { UserRepository } from '../typeorm/repositories/UsersRepository';
@@ -19,10 +20,12 @@ class CreateUserService {
             throw new AppError("Email address already used.");
         }
 
+        const hashedPassword = await hash(password, 8);
+
         const user = usersRepository.create({
             name,
             email,
-            password,
+            password: hashedPassword,
         });
 
         await usersRepository.save(user)
