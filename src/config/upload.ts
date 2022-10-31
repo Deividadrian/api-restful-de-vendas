a7545949497cf0c41a65575648c88460107ed6d1
deividadrian <deividadrian24@gmail.com>
2022-10-30 23:39:04 -0300

    Criado o controller de avatar de usuario

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -11,9 +11,9 @@ export default {
         filename(request, file, callback) {
             const fileHash = crypto.randomBytes(10).toString('hex');
 
-            const fileName = `${fileHash}-${file.originalname}`;
+            const filename = `${fileHash}-${file.originalname}`;
 
-            callback(null, fileName);
+            callback(null, filename);
         }
     })
 }
