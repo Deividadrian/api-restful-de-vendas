94f30c08f54ace9c2f3c43f372500361406ab7ef
deividadrian <deividadrian24@gmail.com>
2022-11-01 23:51:13 -0300

    Adicionando a rota estatica para imagem de avatar

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 import UsersController from '../controllers/UsersController';
 import isAutenticated from '@shared/http/middlewares/isAutenticated';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
+
+const apload = multer(uploadConfig);
 
 usersRouter.get('/', isAutenticated, usersController.index);
 
@@ -52,6 +58,13 @@ usersRouter.delete(
       }
     }),
     usersController.show
+  );
+
+  usersRouter.patch(
+    '/avatar',
+    isAutenticated,
+    apload.single('avatar'),
+    usersAvatarController.update,
   )
 
 export default usersRouter
