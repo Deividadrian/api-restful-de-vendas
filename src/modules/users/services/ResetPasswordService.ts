083ba61322fa0cc252990fddf429364842f28fdc
deividadrian <deividadrian24@gmail.com>
2022-11-07 23:31:56 -0300

    Criado o serviço para redefinição de senha

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,42 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from "typeorm";
+import { hash } from 'bcryptjs';
+import { isAfter, addHours } from 'date-fns'
+import UserRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+    token: string;
+    password: string;
+}
+
+class ResetPasswordService {
+    public async execute({ token, password }: IRequest): Promise<void> {
+        const usersRepository = getCustomRepository(UserRepository);
+        const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+        const userToken = await userTokensRepository.findByToken(token);
+
+        if(!userToken) {
+            throw new AppError('User Token does not exists.')
+        }
+
+        const user = await usersRepository.findById(userToken.user_id);
+
+        if(!user) {
+            throw new AppError('User Token does not exists.')
+        }
+
+        const tokenCreatedAt = userToken.created_at;
+        const compareDate = addHours(tokenCreatedAt, 2);
+
+        if(isAfter(Date.now(), compareDate)) {
+            throw new AppError('Token expired.');
+        }
+
+        user.password = await hash(password, 8);
+
+    }
+}
+
+export default ResetPasswordService;
