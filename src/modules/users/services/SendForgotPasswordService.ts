2fddc203f15dbb36b17a7adbedfdbf61c4cb956d
deividadrian <deividadrian24@gmail.com>
2022-11-07 21:45:24 -0300

    Criado o serviço para senha esquecida

---
+++ src/modules/users/services/SendForgotPasswordService.ts
@@ -0,0 +1,28 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from "typeorm";
+import UserRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+    email: string;
+}
+
+class SendForgotPasswordService {
+    public async execute({ email }: IRequest): Promise<void> {
+        const usersRepository = getCustomRepository(UserRepository);
+        const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+        const user = await usersRepository.findByeEmail(email);
+
+        if(!user) {
+            throw new AppError('User does not exists.')
+        }
+
+        const token = await userTokensRepository.generate(user.id);
+
+        console.log(token);
+
+    }
+}
+
+export default SendForgotPasswordService;
