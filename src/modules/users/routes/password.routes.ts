58996c432289513db76908bc538e66992d8b4b3a
deividadrian <deividadrian24@gmail.com>
2022-11-08 22:55:03 -0300

    Rota de redefinição de senha

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
 
 const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
+
 // http://localhost:3333/password/forgot
 passwordRouter.post(
   '/forgot',
@@ -14,5 +17,18 @@ passwordRouter.post(
   }),
   forgotPasswordController.create,
   );
+
+  // http://localhost:3333/password/reset
+passwordRouter.post(
+    '/reset',
+    celebrate({
+      [Segments.BODY]: {
+          token: Joi.string().uuid().required(),
+          password: Joi.string().required(),
+          password_confirmation: Joi.string().required().valid(Joi.ref('password'))
+      }
+    }),
+    resetPasswordController.create,
+    );
 
 export default passwordRouter;
