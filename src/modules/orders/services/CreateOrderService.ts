5269c16122c1ac8f034f5ec4c0db47a8e2608d6c
deividadrian <deividadrian24@gmail.com>
2022-11-19 21:28:32 -0300

    Serviço para criar pedidos de compras

---
+++ src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,32 @@
+import { ProductRepository } from './../../products/typeorm/repositories/ProductsRepository';
+import CustomersRepository from '@modules/customers/typeorm/repositories/CustomersRepository';
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository, getRepository } from "typeorm";
+import Order from '../typeorm/entities/Order';
+import { OrdersRepository } from '../typeorm/repositories/OredrsRepository';
+
+interface IProduct {
+    id: string;
+    quntity: number
+}
+
+interface IRequest {
+    customer_id: string;
+    products: IProduct[]
+}
+
+class CreateOrderService {
+    public async execute({customer_id, products}: IRequest): Promise<Order> {
+        const ordersRepository = getCustomRepository(OrdersRepository);
+        const customersRepository = getCustomRepository(CustomersRepository);
+        const productsRepository = getRepository(ProductRepository);
+
+        const customersExists = await customersRepository.findById(customer_id);
+
+        if(!customersExists) {
+            throw new AppError('Could not find any customer with the given id');
+        }
+    }
+}
+
+export default CreateOrderService;
