ee381cb2b44e31cd0b91868203f1392b8386ab83
deividadrian <deividadrian24@gmail.com>
2022-11-24 19:39:28 -0300

    Corrigindo erros de tipagem do tipeorm

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -3,17 +3,18 @@ import Customer from '../typeorm/entities/Customer';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 
 interface IPaginateCustomer {
-  from: number;
-  to: number;
-  per_page: number;
-  total: number;
-  current_page: number;
-  prev_page: number | null;
-  next_page: number | null;
-  data: Customer[];
-}
+    from: number;
+    to: number;
+    per_page: number;
+    total: number;
+    current_page: number;
+    prev_page: number | null;
+    next_page: number | null;
+    data: Customer[];
+  }
+
 class ListCustomerService {
-  public async execute(): Promise<IPaginateCustomer> {
+   public async execute(): Promise<IPaginateCustomer> {
     const customersRepository = getCustomRepository(CustomersRepository);
 
     const customers = await customersRepository.createQueryBuilder().paginate();
